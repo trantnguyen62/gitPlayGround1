add746419d89e374622a5ed2fa5a2c14eb7d51cd
Tran Nguyen <nguyenthachtran@gmail.com>
2024-12-20 12:00:00 -0600

    check 1

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,7 +4,11 @@ public class HomePage {
 
 
     public static void main(String[] args) {
+<<<<<<< HEAD
             System.out.println("Hi    ");
+=======
+            System.out.println("Hello    ");
+>>>>>>> e5c2510 (check 1)
     }
 
 
